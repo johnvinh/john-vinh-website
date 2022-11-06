106bf3451b5c3ae6d32a088219f88fc8ed0c7b5d
John Vinh <john.michael.vinh2@gmail.com>
2022-11-06 13:37:50 -0500

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,19 @@
 </script>
 
 <header>
-    <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">John Vinh</h1>
-        <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">I'm a software developer and a computer science student!</p>
+    <nav class="navbar navbar-expand">
+        <div class="container">
+            <a href="#" class="navbar-brand">John Vinh</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+            </ul>
         </div>
-    </div>
+    </nav>
 </header>
 <main>
     <slot></slot>
