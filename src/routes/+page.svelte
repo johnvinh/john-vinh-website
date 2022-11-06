b41f2b0ffa30a686ba10369cf2205a56899cb081
John Vinh <john.michael.vinh2@gmail.com>
2022-11-06 13:37:46 -0500

    Move main hero to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 
 </script>
 
-<main>
-    
-</main>
+<div class="px-4 py-5 my-5 text-center" id="">
+    <h1 class="display-5 fw-bold">John Vinh</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">I'm a software developer and a computer science student!
+            I love building things for other people to enjoy. I also love coffee. ☕</p>
+    </div>
+</div>
