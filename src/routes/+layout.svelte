db19a49bea3e9d00f7bddd08d381d15cfd546aa1
John Vinh <john.michael.vinh2@gmail.com>
2022-11-06 13:21:06 -0500

    Import bootstrap and create basic header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
-
+    import '$lib/bootstrap.min.css';
 </script>
 
 <header>
-    <h1>John Vinh</h1>
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold">John Vinh</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">I'm a software developer and a computer science student!</p>
+        </div>
+    </div>
 </header>
+<main>
+    <slot></slot>
+</main>
+
+<style>
+
+</style>
