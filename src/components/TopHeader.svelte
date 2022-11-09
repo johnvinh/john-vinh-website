0ed8062d15a3389322a2e6f8e0c109792ef5bbfc
John Vinh <john.michael.vinh2@gmail.com>
2022-11-09 09:29:47 -0500

    Made code more modular with components

---
+++ src/components/TopHeader.svelte
@@ -0,0 +1,15 @@
+<div class="px-4 py-5 text-center" id="top-header">
+    <h1 class="display-5 fw-bold">John Vinh</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">I'm a software developer and a computer science student!
+            I love building things for other people to enjoy. I also love coffee. ☕</p>
+    </div>
+</div>
+
+<style>
+    #top-header {
+        background-color: #C2D076;
+        color: #000;
+        margin: 0;
+    }
+</style>
