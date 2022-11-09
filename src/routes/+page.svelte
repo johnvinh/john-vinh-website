7474247f526e3d832a511b96fd94987a4ddfd1dd
John Vinh <john.michael.vinh2@gmail.com>
2022-11-09 09:18:38 -0500

    Remove annoying margin from top hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<div class="px-4 py-5 my-5 text-center" id="top-header">
+<div class="px-4 py-5 text-center" id="top-header">
     <h1 class="display-5 fw-bold">John Vinh</h1>
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">I'm a software developer and a computer science student!
@@ -12,6 +12,8 @@
 
 <style>
     #top-header {
+        background-color: #129490;
         color: #000;
+        margin: 0;
     }
 </style>
