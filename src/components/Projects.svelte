a283aa7408199e6bef926e4f895bd148f82e20a7
John Vinh <john.michael.vinh2@gmail.com>
2022-11-10 11:47:03 -0500

    Add technologies used for GloomyBB and their images

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -8,10 +8,17 @@
                 </div>
                 <h3 class="fs-2">GloomyBB</h3>
                 <p>A forum software designed for ease of communication among users. Contains typical forum features such as creation of categories, forums, posts, topics, and users.</p>
-                <a href="/" class="icon-link d-inline-flex align-items-center">
-                    Call to action
-                    <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"/></svg>
-                </a>
+                <img src="/images/php-logo.svg" alt="PHP" width="50" height="50">
+                <img src="/images/mariadb-logo.svg" alt="MariaDB" width="50" height="50">
+                <img src="/images/html5-logo.svg" alt="HTML5" width="50" height="50">
+                <img src="/images/css-logo.svg" alt="CSS" width="50" height="50">
+                <img src="/images/js-logo.svg" alt="JavaScript" width="50" height="50">
+                <p>
+                    <a href="https://github.com/johnvinh/gloomybb" class="icon-link d-inline-flex align-items-center">
+                        GitHub Repository
+                        <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"/></svg>
+                    </a>
+                </p>
             </div>
             <div class="feature col">
                 <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
