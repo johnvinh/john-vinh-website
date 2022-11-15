f36d48e33787c371245251dd9370831a839e322f
John Vinh <john.michael.vinh2@gmail.com>
2022-11-15 16:09:28 -0500

    Setup Tailwind for use with SvelteKit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,8 @@
 <script>
-    import '$lib/bootstrap.min.css';
-    import '$lib/bootstrap.css';
-    import '$lib/style.css';
+    import "../app.css";
 </script>
 
-<header>
-    <nav class="navbar navbar-expand bg-secondary bg-gradient">
-        <div class="container">
-            <a href="/" class="navbar-brand">John Vinh</a>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Blog</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
-<main>
-    <slot></slot>
-</main>
-<footer>
-    <div class="container">
-        <p>Copyright &copy; 2022 John Vinh</p>
-    </div>
-</footer>
+
 
 <style>
     a {
