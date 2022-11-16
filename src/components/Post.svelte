4ecb782ee39440a597dbaf46f6d33b4ae950d3a3
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 11:55:40 -0500

    Create Post component

---
+++ src/components/Post.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Box from './Box.svelte';
+
+    export let post;
+    let date = new Date(post.created);
+    let prettyDate =
+        date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+</script>
+
+<div class="mt-5">
+    <h2 class="text-4xl">{post.title}</h2>
+    <p>{prettyDate}</p>
+</div>
