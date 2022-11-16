8efff3695d15bbb583f253f0bbbc1858288e8bfd
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 11:39:11 -0500

    Remove unnecessary content container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,29 +5,28 @@
 <svelte:head>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu">
 </svelte:head>
-<div id="content">
-    <header class="text-black p-5">
-        <nav class="flex flex-row justify-evenly">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/blog">Blog</a>
-        </nav>
-    </header>
-    <main class="w-1/2 mx-auto">
-        <slot></slot>
-    </main>
-    <footer class="flex flex-row mt-10 w-1/2 mx-auto justify-center">
-        <a href="https://www.linkedin.com/in/john-vinh/">
-            <img src="/images/linkedin-logo.svg" alt="LinkedIn logo" class="w-[50px] h-[50px]">
-        </a>
-        <a href="https://twitter.com/JohnVinhDev">
-            <img src="/images/twitter-logo.svg" alt="Twitter logo" class="w-[50px] h-[50px]">
-        </a>
-        <a href="https://github.com/johnvinh">
-            <img src="/images/github-logo.svg" alt="GitHub logo" class="w-[50px] h-[50px]">
-        </a>
-    </footer>
-</div>
+
+<header class="text-black p-5">
+    <nav class="flex flex-row justify-evenly">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+    </nav>
+</header>
+<main class="w-1/2 mx-auto">
+    <slot></slot>
+</main>
+<footer class="flex flex-row mt-10 w-1/2 mx-auto justify-center">
+    <a href="https://www.linkedin.com/in/john-vinh/">
+        <img src="/images/linkedin-logo.svg" alt="LinkedIn logo" class="w-[50px] h-[50px]">
+    </a>
+    <a href="https://twitter.com/JohnVinhDev">
+        <img src="/images/twitter-logo.svg" alt="Twitter logo" class="w-[50px] h-[50px]">
+    </a>
+    <a href="https://github.com/johnvinh">
+        <img src="/images/github-logo.svg" alt="GitHub logo" class="w-[50px] h-[50px]">
+    </a>
+</footer>
 
 <style>
     :global(body) {
