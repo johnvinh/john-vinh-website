1d9134c7357ec9d8ed15502211a9d5b8288e3f39
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 12:32:37 -0500

    Add function to retrieve a single post in api

--- src/lib/api.js
+++ src/lib/api.js
@@ -10,3 +10,12 @@ export async function getPosts()
         sort: '-created',
     });
 }
+
+export async function getPostById(id)
+{
+    const email = import.meta.env.VITE_POCKETBASE_EMAIL;
+    const password = import.meta.env.VITE_POCKETBASE_PASSWORD;
+    const client = new PocketBase("http://127.0.0.1:8090");
+    await client.admins.authViaEmail(email, password);
+    return await client.records.getOne('posts', id);
+}
