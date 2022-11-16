6c7c707f484e4c12b572ca5c7d193f9862a2fdd3
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 12:32:55 -0500

    Display post using url param

---
+++ src/routes/blog/post/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Title from '../../../components/Title.svelte';
+    import {getPostById} from '$lib/api.js';
+    import {page} from '$app/stores';
+
+    let id = $page.url.searchParams.get('id');
+
+    let post;
+
+    (async () => {
+        post = await getPostById(id);
+    })();
+</script>
+
+{#if post}
+    <Title>
+        {post.title}
+    </Title>
+    <p>{post.content}</p>
+{/if}
