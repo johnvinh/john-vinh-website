d975fbd3f5a4989d32b6ee371e6534ddbb05af8a
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 14:48:36 -0500

    Add underline to subtitles

--- src/components/Subtitle.svelte
+++ src/components/Subtitle.svelte
@@ -1,3 +1,10 @@
 <h2 class="text-4xl mt-10">
     <slot></slot>
 </h2>
+
+<style>
+    h2 {
+        border-bottom: #E63946 3px solid;
+        display: inline-block;
+    }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,19 +7,25 @@
     About Me
 </Title>
 
-<Subtitle>
-    Early On
-</Subtitle>
-<p>
-I've been interested in building since I was a child. I started off following YouTube tutorials on how to make
-basic websites with just HTML in the early 2010s.I followed up on my interest by learning Python while I was in high school. I used it to automate some
-tedious tasks.
-</p>
-<Subtitle>
-    Now
-</Subtitle>
-<p>
-    I'm currently a student at Algonquin College in the Computer Engineering Technology - Computing Science program.
-    I recently learned I have a passion for making things for other people to have fun with and enjoy after watching
-    the "Minecraft Championship" event on YouTube. I spend a lot of time developing Minecraft plugins.
-</p>
+<div>
+    <section>
+        <Subtitle>
+            Early On
+        </Subtitle>
+        <p>
+            I've been interested in building since I was a child. I started off following YouTube tutorials on how to make
+            basic websites with just HTML in the early 2010s.I followed up on my interest by learning Python while I was in high school. I used it to automate some
+            tedious tasks.
+        </p>
+    </section>
+    <section>
+        <Subtitle>
+            Now
+        </Subtitle>
+        <p>
+            I'm currently a student at Algonquin College in the Computer Engineering Technology - Computing Science program.
+            I recently learned I have a passion for making things for other people to have fun with and enjoy after watching
+            the "Minecraft Championship" event on YouTube. I spend a lot of time developing Minecraft plugins.
+        </p>
+    </section>
+</div>
