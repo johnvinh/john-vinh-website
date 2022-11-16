5912b0d7eab35fb8b9a5f013e8eed67acba096de
John Vinh <john.michael.vinh2@gmail.com>
2022-11-16 11:24:55 -0500

    Create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {page} from "$app/stores";
+
+</script>
+
+<h2 class="text-4xl">
+    Oops, something went wrong.
+</h2>
+<p>
+    Click <a href="/">here</a> to go back to the homepage!
+</p>
