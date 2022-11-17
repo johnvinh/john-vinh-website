b789d2cf2aa8cdcebb742cb40cafa26266abcd69
John Vinh <john.michael.vinh2@gmail.com>
2022-11-17 15:15:00 -0500

    Improve security

--- src/routes/blog/create/+page.server.js
+++ src/routes/blog/create/+page.server.js
@@ -2,18 +2,19 @@ import {createPost, login} from '$lib/api.js';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    createPost: async (event) => {
+    default: async (event) => {
+        // Require the user to be logged in
         const data = await event.request.formData();
+        const email = data.get('email');
+        const password = data.get('password');
+        if (!await login(email, password)) {
+            return {
+                failedLogin: true
+            };
+        }
         const title = data.get('title');
         const content = data.get('content');
         const result = await createPost(title, content);
         return {success: result}
-    },
-    login: async (event) => {
-        const data = await event.request.formData();
-        const email = data.get('email');
-        const password = data.get('password');
-        const result = await login(email, password);
-        return {loggedin: result}
     }
 };
--- src/routes/blog/create/+page.svelte
+++ src/routes/blog/create/+page.svelte
@@ -6,48 +6,42 @@
     export let form;
 </script>
 
-{#if form?.loggedin}
-    <Title>
-        Make A Post
-    </Title>
-    {#if form?.success}
-        <p>Your post has been submitted!</p>
-    {/if}
-    {#if form?.success === false}
-        <p>There was an error submitting the post.</p>
-    {/if}
-    <form class="flex flex-col" method="POST" action="?/createPost">
-        <label for="title" class="block">
-            <Subtitle>
-                Title
-            </Subtitle>
-        </label>
-        <input class="block w-full" type="text" name="title" id="title" placeholder="Title" />
-        <label class="block" for="content">
-            <Subtitle>
-                Content
-            </Subtitle>
-        </label>
-        <textarea class="block w-full h-screen" name="content" id="content" placeholder="Content"></textarea>
-        <button class="bg-themered text-white p-5 mt-10 rounded-2xl" type="submit">Submit</button>
-    </form>
-{:else}
-    <Title>
-        Login
-    </Title>
-    <form class="flex flex-col" method="POST" action="?/login">
-        <label for="email" class="block">
-            <Subtitle>
-                Email
-            </Subtitle>
-        </label>
-        <input class="block w-full" type="text" name="email" id="email" placeholder="Email" />
-        <label class="block" for="password">
-            <Subtitle>
-                Password
-            </Subtitle>
-        </label>
-        <input class="block w-full" type="password" name="password" id="password" placeholder="Password" />
-        <button class="bg-themered text-white p-5 mt-10 rounded-2xl" type="submit">Login</button>
-    </form>
+<Title>
+    Make A Post
+</Title>
+{#if form?.success}
+    <p>Your post has been submitted!</p>
 {/if}
+{#if form?.success === false}
+    <p>There was an error submitting the post.</p>
+{/if}
+{#if form?.failedLogin}
+    <p>Invalid credentials.</p>
+{/if}
+<form class="flex flex-col" method="POST">
+    <label for="email" class="block">
+        <Subtitle>
+            Email
+        </Subtitle>
+    </label>
+    <input class="block" type="text" name="email" id="email" placeholder="Email" required>
+    <label for="password" class="block">
+        <Subtitle>
+            Password
+        </Subtitle>
+    </label>
+    <input class="block" type="password" name="password" id="password" placeholder="Password" required>
+    <label for="title" class="block">
+        <Subtitle>
+            Title
+        </Subtitle>
+    </label>
+    <input class="block w-full" type="text" name="title" id="title" placeholder="Title" />
+    <label class="block" for="content">
+        <Subtitle>
+            Content
+        </Subtitle>
+    </label>
+    <textarea class="block w-full h-screen" name="content" id="content" placeholder="Content"></textarea>
+    <button class="bg-themered text-white p-5 mt-10 rounded-2xl" type="submit">Submit</button>
+</form>
