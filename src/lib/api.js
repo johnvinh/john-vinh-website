d65e1b0c47fc7a7f477744cdc64f21292309b71e
John Vinh <john.michael.vinh2@gmail.com>
2022-11-17 11:59:45 -0500

    Replace host with env variable to make deploying to production easier

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,8 @@ export async function getPosts()
 {
     const email = import.meta.env.VITE_POCKETBASE_EMAIL;
     const password = import.meta.env.VITE_POCKETBASE_PASSWORD;
-    const client = new PocketBase("http://127.0.0.1:8090");
+    const host = import.meta.env.VITE_POCKETBASE_HOST;
+    const client = new PocketBase(`${host}:8090`);
     await client.admins.authViaEmail(email, password);
     return await client.records.getFullList('posts', 200, {
         sort: '-created',
@@ -15,7 +16,8 @@ export async function getPostById(id)
 {
     const email = import.meta.env.VITE_POCKETBASE_EMAIL;
     const password = import.meta.env.VITE_POCKETBASE_PASSWORD;
-    const client = new PocketBase("http://127.0.0.1:8090");
+    const host = import.meta.env.VITE_POCKETBASE_HOST;
+    const client = new PocketBase(`${host}:8090`);
     await client.admins.authViaEmail(email, password);
     let post;
     try {
