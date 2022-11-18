2b5edcce08887cb874a358e5092bb1057b135656
John Vinh <john.michael.vinh2@gmail.com>
2022-11-17 20:16:06 -0500

    Add more info to about me

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,9 +14,20 @@
             Early On
         </Subtitle>
         <p>
-            I've been interested in building since I was a child. I started off following YouTube tutorials on how to make
-            basic websites with just HTML in the early 2010s.I followed up on my interest by learning Python while I was in high school. I used it to automate some
-            tedious tasks.
+            I've been interested in building since I was around 10 years old. I started off following YouTube tutorials
+            on how to make basic websites with just HTML in the early 2010s. I followed up on my interest by learning
+            Python while I was in high school using various sources like Codecademy and YouTube videos. I used Python
+            to automate some tedious tasks and make scripts for some online browser games I play.
+        </p>
+    </section>
+    <section>
+        <Subtitle>High School</Subtitle>
+        <p>
+            In high school, I took a computer science class starting in grade 10. We used Visual Basic and
+            ActionScript, some not-so-popular languages. I loved it and I ended up taking computer science classes
+            in grade 11 and 12 as well. In each of these classes, I had a final project that involved working with
+            3rd grade students and using their ideas and drawings to create a flash game with ActionScript. It was in
+            these classes where my love for programming really started to grow.
         </p>
     </section>
     <section>
@@ -26,7 +37,8 @@
         <p>
             I'm currently a student at Algonquin College in the Computer Engineering Technology - Computing Science program.
             I recently learned I have a passion for making things for other people to have fun with and enjoy after watching
-            the "Minecraft Championship" event on YouTube. I spend a lot of time developing Minecraft plugins.
+            the "Minecraft Championship" event on YouTube. I'm hoping to get a job in the tech industry as a
+            software developer after I graduate.
         </p>
     </section>
 </div>
