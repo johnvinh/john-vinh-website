968e4b3456cd838e9fac8e41b77cd8433726b023
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 13:46:09 -0500

    Remove projects from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
     import TopHeader from '../components/TopHeader.svelte';
     import Projects from '../components/Projects.svelte';
+    import Subtitle from '../components/Subtitle.svelte';
 </script>
 
 <svelte:head>
     <title>John Vinh: Software Developer</title>
 </svelte:head>
 <TopHeader />
-<Projects />
+<Subtitle>A little about me</Subtitle>
+<p>I love building things for other people to enjoy and enjoy the thrill of solving problems.</p>
+<Subtitle>More</Subtitle>
+<ul>
+    <li class="text-3xl">Read more <a href="/about">about me!</a></li>
+    <li class="text-3xl">Check out all of <a href="/projects">my projects!</a></li>
+</ul>
