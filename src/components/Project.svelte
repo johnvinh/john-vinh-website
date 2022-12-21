d560dacc87117d674564e32a4a5ad6216fe12c45
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 14:17:03 -0500

    Improve project styling

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -9,7 +9,7 @@
     export let link;
 </script>
 
-<div class="mt-10">
+<div class="mt-10 box">
     <img src="/images/{titleImage}" alt="{title}" class="w-1/2 mx-auto">
     <img src="/images/{mainImage}" class="w-1/3 mx-auto" alt="{title} logo">
     <h3 class="text-2xl text-center">{title}</h3>
@@ -22,3 +22,43 @@
         {/each}
     </div>
 </div>
+
+<style>
+    .box {
+        background-color: #f5f5f5;
+        border-radius: 10px;
+        padding: 1rem;
+    }
+
+    .box:hover {
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .box img {
+        border-radius: 10px;
+    }
+
+    .box h3 {
+        margin-top: 1rem;
+    }
+
+    .box p {
+        margin-top: 1rem;
+    }
+
+    .box a {
+        color: #000;
+    }
+
+    .box a:hover {
+        color: #000;
+    }
+
+    .box a:visited {
+        color: #000;
+    }
+
+    .box a:active {
+        color: #000;
+    }
+</style>
