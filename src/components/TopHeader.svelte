a642fd088e537a681040dcf82f3898401f3f44b7
John Vinh <john.michael.vinh2@gmail.com>
2022-12-22 12:30:21 -0500

    Reduce main header text size on mobile

--- src/components/TopHeader.svelte
+++ src/components/TopHeader.svelte
@@ -7,7 +7,7 @@
     <Title>
         Hi, I'm John! 👋
     </Title>
-    <p class="text-3xl">
+    <p class="md:text-3xl">
         I'm a computer engineering college student and software developer.
         I love making things for other people to enjoy! I also love coffee ☕
     </p>
