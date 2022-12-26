23283e329314bb645862c5be9f93e40ba950e0b6
John Vinh <john.michael.vinh2@gmail.com>
2022-12-26 13:00:18 -0500

    Remove homepage extra info and add margin bottom to first list item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,7 @@
     <title>Home - John Vinh: Software Developer</title>
 </svelte:head>
 <TopHeader />
-<Subtitle>A little about me</Subtitle>
-<p>I love building things for others to enjoy and the thrill of solving problems.</p>
-<Subtitle>More</Subtitle>
-<ul>
-    <li class="font-bold md:text-3xl">Read more <a href="/about">about me!</a></li>
+<ul class="my-10">
+    <li class="font-bold md:text-3xl mb-5">Read more <a href="/about">about me!</a></li>
     <li class="font-bold md:text-3xl">Check out all of <a href="/projects">my projects!</a></li>
 </ul>
