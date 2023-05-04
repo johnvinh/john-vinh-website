2e72e3660cbccc3409d06d668b396286b294a47f
John Vinh <john.michael.vinh2@gmail.com>
2023-05-04 12:20:39 -0400

    Add one in the chamber project

---
+++ src/components/MinecraftProject.svelte
@@ -0,0 +1,55 @@
+<script>
+    import Box from './Box.svelte';
+
+    export let title;
+    export let description;
+    export let image;
+    export let link;
+</script>
+
+<div class="mt-10 box">
+    <img src="{image}" alt="{title}" class="w-full">
+    <h3 class="text-xl font-bold">{title}</h3>
+    <a href="{link}" target="_blank">GitHub Repository</a>
+    <p>{description}</p>
+</div>
+
+<style>
+    .box {
+        background-color: #f5f5f5;
+        border-radius: 10px;
+        padding: 1rem;
+    }
+
+    .box:hover {
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .box img {
+        border-radius: 10px;
+    }
+
+    .box h3 {
+        margin-top: 1rem;
+    }
+
+    .box p {
+        margin-top: 1rem;
+    }
+
+    .box a {
+        color: #000;
+    }
+
+    .box a:hover {
+        color: #000;
+    }
+
+    .box a:visited {
+        color: #000;
+    }
+
+    .box a:active {
+        color: #000;
+    }
+</style>
---
+++ src/routes/minecraft/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Project from '/src/components/Project.svelte';
+    import Title from '../../components/Title.svelte';
+    import MinecraftProject from '../../components/MinecraftProject.svelte';
+</script>
+
+<svelte:head>
+    <title>Minecraft - John Vinh: Software Developer</title>
+</svelte:head>
+
+<section class="mt-10">
+    <Title>Minecraft</Title>
+    <p>Welcome to the Minecraft corner of my portfolio! As a passionate software developer and an avid Minecraft enthusiast, I discovered I love creating Minecraft content. Inspired by the incredible Noxcrew's MC Championship event and fun with friends playing on the Hypixel server, I've been captivated by the limitless potential for creativity and innovation in the world of Minecraft minigames. Below you can find some of the Minecraft content I've created.</p>
+</section>
+<section class="flex flex-col">
+    <MinecraftProject
+        title="One in the Chamber"
+        description="A recreation of the classic Minecraft minigame One in the Chamber, where players have one arrow and one life. If you kill a player, you get an arrow. If you get killed, you lose an arrow. The first player to get 20 kills wins!"
+        image="/images/one-in-the-chamber.png"
+        link="https://github.com/johnvinh/one-in-the-chamber"
+    />
+</section>
