f49658e58404010b40361f9d7350dc37c5be4266
John Vinh <john.michael.vinh2@gmail.com>
2023-06-15 22:20:35 -0400

    Improve responsiveness on new design

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -1,6 +1,6 @@
-<header class="text-black p-5 grid grid-cols-12">
-    <span class="text-white font-bold col-span-8">John Vinh</span>
-    <nav class="inline-flex flex-row justify-evenly md:text-2xl font-bold col-span-4">
+<header class="text-black p-5 flex flex-col lg:grid lg:grid-cols-12">
+    <span class="text-white font-bold lg:col-span-6 text-center lg:text-left">John Vinh</span>
+    <nav class="inline-flex text-center flex-col md:flex-row justify-evenly md:text-2xl font-bold lg:col-span-6">
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/projects">Projects</a>
