53e1062885fb1a98ee3f7f24935d6387a479b913
John Vinh <john.michael.vinh2@gmail.com>
2023-07-19 18:40:27 -0400

    Fix navigation bar on mobile devices

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -1,6 +1,6 @@
-<header class="bg-gray-800 text-white p-5 grid grid-cols-12">
-    <span class="text-2xl font-bold col-span-8">John Vinh</span>
-    <nav class="inline-flex flex-row justify-evenly text-lg font-bold col-span-4 space-x-4">
+<header class="bg-gray-800 text-white p-5 flex flex-col md:grid md:grid-cols-12 sm text-center md:text-left">
+    <span class="text-2xl font-bold md:col-span-4 lg:col-span-8">John Vinh</span>
+    <nav class="inline-flex flex-col md:flex-row justify-evenly text-lg font-bold md:col-span-8 lg:col-span-4 space-x-4">
         <a href="/" class="hover:text-blue-500">Home</a>
         <a href="/about" class="hover:text-blue-500">About</a>
         <a href="/projects" class="hover:text-blue-500">Projects</a>
