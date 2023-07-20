e188bddf4ae09f5eb33c5152547b81400596ad1e
John Vinh <john.michael.vinh2@gmail.com>
2023-07-20 10:05:25 -0400

    Install and initialize TinyMCE editor

---
+++ src/components/Editor.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
+
+    /*import tinymce from 'tinymce/tinymce';
+    import 'tinymce/themes/silver';
+    import 'tinymce/plugins/link';
+    import 'tinymce/plugins/table';*/
+
+    let textarea;
+
+    onMount(() => {
+        if (browser) {
+            import('tinymce/tinymce').then(module => {
+                const tinymce = module.default;
+                import('tinymce/themes/silver');
+                import('tinymce/plugins/link');
+                import('tinymce/plugins/table');
+
+                tinymce.init({
+                    target: textarea,
+                    plugins: ['link', 'table'], // add more plugins as needed
+                    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link table', // customize the toolbar as needed
+                    base_url: '/node_modules/tinymce', // point to tinymce in node_modules
+                    suffix: '', // no suffix for modern browsers
+                    // add more options as needed
+                });
+            });
+        }
+    });
+
+</script>
+
+<textarea class="block w-full h-screen" name="content" id="content" bind:this={textarea}></textarea>
